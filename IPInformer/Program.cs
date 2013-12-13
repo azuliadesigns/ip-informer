af658e59700cd17b28ad9bef502d399d2bb20d21
techmantium <git@ninjamail.me>
2013-12-13 12:06:46 +0000

    Initial commit
    Initial commit of current code base

---
+++ IPInformer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace IPInformer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SysTrayApplication());
+        }
+    }
+
+
+
+    public class SysTrayApplication : ApplicationContext
+    {
+        private NotifyIcon _trayIcon;
+        private ContextMenuStrip _menu;
+        private ToolStripMenuItem _exitMenu;
+
+        public SysTrayApplication()
+        {
+            _trayIcon = new NotifyIcon();
+            _trayIcon.Icon = IPInformer.Properties.Resources.icon;
+            _trayIcon.Text = GetIPAddress();
+            _trayIcon.Visible = true;
+
+            _exitMenu = new ToolStripMenuItem("Exit");
+            _exitMenu.Click += new EventHandler(ExitMenu_Click);
+
+            _menu = new ContextMenuStrip();
+            _menu.Items.Add(_exitMenu);
+            _trayIcon.ContextMenuStrip = _menu;
+
+            Application.ThreadExit += new EventHandler(Application_ThreadExit);
+        }
+
+        private void  Application_ThreadExit(object sender, EventArgs e)
+        {
+ 	        _trayIcon.Visible = false;
+        }
+
+        private void ExitMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private string GetIPAddress()
+        {
+            StringBuilder localIP = new StringBuilder();
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP.AppendLine(ip.ToString());
+                }
+            }
+
+            return localIP.ToString();
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,34 @@
+IP Informer: A Desktop Support Tool
+
+Tim Trott - http://techmantium.com - http://timtrott.co.uk
+
+SUMMARY:
+  
+* Give your users an easy way to help IT support staff by providing them with the easiest IP Address lookup tool.
+* Simple system tray application that performs an IP address lookup for all LAN connections
+
+DESCRIPTION:
+
+IP Address Informer is a simple system tray application that performs an IP address lookup for all LAN connections. IP Address Informer will show the current IP address of the users machine so that they can let IT support staff know the IP Address to connect to remotely through Remote Desktop or VNC. No more talking users through using ipconfig or navigating network properties to lookup IP address!
+You can also use this tool to check IP address assigned to your computer and to find your IP address.
+
+REQUIREMENTS:
+
+* Visual Studio 2010 or later to compile
+* Windows XP (or newer) with .Net Framework to run
+
+INSTRUCTIONS:
+
+* Using IP Address Informer is as easy as asking your users to place their mouse cursor over the thunderbolt icon on the bottom right hand side of the screen next to the clock! It will then popup a tooltip stating their local IP address on the internal LAN.
+* To compile the executable, open IPInformer.sln in Visual Studio and build the project.
+
+LICENSE:
+
+GNU GENERAL PUBLIC LICENSE
+
+IP Informer: A Desktop Support Tool
+Copyright (c) 2013 Tim Trott
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
